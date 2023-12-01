35e31d98af0091a1d4221aa98198e56ed807b520
Niklas Augustsson <80054533+7Sovaren@users.noreply.github.com>
2023-12-01 13:35:57 +0100

    red

--- 01/trebuchet/src/main.rs
+++ 01/trebuchet/src/main.rs
@@ -33,11 +33,16 @@ mod test{
   use super::*;
 
   #[test]
-  fn create_empty(){
+  fn read_examples(){
     let lines = read_lines("example");
     assert!(lines != None);
     let lines = lines.unwrap();
 
     assert!(lines[0] == "1abc2");
+    assert!(lines[1] == "pqr3stu8vwx");
+    assert!(lines[2] == "a1b2c3d4e5f");
+    assert!(lines[3] == "treb7uchet");
   }
+  
+
 }
